3f2a4103aae331df8fc41a2f3778be56f4bc89ff
tiregram <ruhtra.mar@gmail.com>
2017-01-09 10:22:46 +0100

    first

--- BluetoothGlove/app/src/main/java/com/tiregram/glove/bluetoothglove/GloveConnect.java
+++ BluetoothGlove/app/src/main/java/com/tiregram/glove/bluetoothglove/GloveConnect.java
@@ -2,7 +2,6 @@ package com.tiregram.glove.bluetoothglove;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class GloveConnect extends AppCompatActivity {
 
@@ -10,20 +9,5 @@ public class GloveConnect extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_glove_connect);
-
-    // Example of a call to a native method
-    TextView tv = (TextView) findViewById(R.id.sample_text);
-    tv.setText(stringFromJNI());
-    }
-
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
-
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
     }
 }
